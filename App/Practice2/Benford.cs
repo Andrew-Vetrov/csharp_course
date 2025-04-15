1208766343b2d92bb1194571cdc80b86dce784b5
Andrew_Vetrov <user120715mts@gmail.com>
2025-04-15 07:51:30 +0300

    Task 1

---
+++ App/Practice2/Benford.cs
@@ -0,0 +1,44 @@
+namespace App.Practice2;
+
+public class Benford
+{
+    public static int[] GetBenfordStatistics(string text)
+    {
+        var statistics = new int[10];
+        var words = text.Split(' ');
+	
+        foreach (var word in words)
+        {
+            int indx = 0;
+            var len = word.Length;
+            var numbers = "0123456789".ToCharArray();
+
+            while (indx < len)
+            {
+                indx = word.IndexOfAny(numbers, indx);
+                if (indx == -1)
+                {
+                    break;
+                }
+
+                while (indx < len && word[indx] == '0')
+                {
+                    indx++;
+                }
+
+                if (indx < len && Char.IsNumber(word[indx]))
+                {
+                    statistics[word[indx] - '0']++;
+                }
+
+                while (indx < len && Char.IsNumber(word[indx]))
+                {
+                    indx++;
+                }
+            }
+        }
+	    
+        
+        return statistics;
+    }
+}
