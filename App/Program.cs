ef23c2774350507188215b916c1016aef9ba1624
Andrew_Vetrov <user120715mts@gmail.com>
2025-04-22 14:42:15 +0300

    add Benford

--- App/Program.cs
+++ App/Program.cs
@@ -1,9 +1,11 @@
-﻿namespace App;
+﻿using App.Practice2;
+
+namespace App.Practice2;
 
 public static class Program
 {
     public static void Main()
     {
-        Console.WriteLine("Hello, World!");
+        
     }
 }
--- AppTests/Practice2/BenfordTests.cs
+++ AppTests/Practice2/BenfordTests.cs
@@ -8,7 +8,7 @@ public class BenfordTests
     [TestCase("", new int[] {0, 0, 0, 0, 0, 0, 0, 0, 0, 0})]
     [TestCase("0 1 2 3 4 5 6 7 8 9", new int[] {0, 1, 1, 1, 1, 1, 1, 1, 1, 1})]
     [TestCase("00000dsaw 000 0 saqwdw w0 w9", new int[] {0, 1, 0, 0, 0, 0, 0, 0, 0, 1})]
-    public void TestPasses_When_Result_Correct(string text, int[] expected)
+    public void TestFunction(string text, int[] expected)
     {
         var actual = Benford.GetBenfordStatistics(text);
         Assert.That(actual, Is.EqualTo(expected));
