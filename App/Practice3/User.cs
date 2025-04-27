3a0f3fe9fba69fae3f6424248a8fb4d06af09c9c
Andrew_Vetrov <user120715mts@gmail.com>
2025-04-27 10:20:58 +0300

    Practice 3.1

---
+++ App/Practice3/User.cs
@@ -0,0 +1,56 @@
+using System.Text.RegularExpressions;
+
+namespace App.Practice3;
+
+public class User
+{
+    private readonly Guid id;
+    private readonly DateTime registerDate;
+    
+    private string login;
+    private string password;
+    private string name;
+    private string surname;
+    private string inn;
+    private string phone;
+
+    public User(string login, string password, string name, string surname, string inn, string phone)
+    {
+        this.id = Guid.NewGuid();
+        this.registerDate = DateTime.Now;
+        this.login = login;
+        this.password = password;
+        this.name = name;
+        this.surname = surname;
+        this.inn = inn;
+        this.phone = phone;
+    }
+    
+    private static bool IsPhoneValid(string inputString) 
+    {
+        MatchCollection matches = new Regex(@"^(?:\+7|8|7)?[\s\-]?\(?\d{3}\)?[\s\-]?\d{3}[\s\-]?\d{2}[\s\-]?\d{2}$").Matches(inputString);
+
+        if (matches.Count != 1 || matches[0].Value.Length != inputString.Length) // второе условие - проверка на то, что в строке нет ничего лишнего
+        {
+            return false;
+        }
+
+        return true;
+    }
+
+    public bool TryUpdatePhone(string phone)
+    {
+        if (IsPhoneValid(phone))
+        {
+            this.phone = phone;
+            return true;
+        }
+        
+        return false;
+    }
+    
+    public string GetUserFullName()
+    {
+        return $"{name} {surname}";
+    }
+}
--- App/Program.cs
+++ App/Program.cs
@@ -1,9 +1,15 @@
-﻿namespace App;
+﻿using System.Diagnostics;
+using App.Practice3;
+
+namespace App;
 
 public static class Program
 {
     public static void Main()
     {
-        Console.WriteLine("Hello, World!");
+        var user1 = new User("vanya", "qwerty123", "Ivan", "Ivanov", "0948032480", "+7 987 654 32 10");
+        var user2 = new User("ahah", "hehe", "Andrew", "Vetrov", "1234567890", "+77777777777");
+        Debug.Assert(user1.TryUpdatePhone("88005553535"));
+        Console.WriteLine(user2.GetUserFullName());
     }
 }
