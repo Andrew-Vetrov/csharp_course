5c3a3f23299c7aa334269dbb2b329080e903af4c
Andrew_Vetrov <user120715mts@gmail.com>
2025-04-28 06:58:36 +0300

    Practice 3.3

--- App/Program.cs
+++ App/Program.cs
@@ -1,9 +1,12 @@
-﻿namespace App;
+﻿using App.Practice3;
+
+namespace App;
 
 public static class Program
 {
     public static void Main()
     {
-        Console.WriteLine("Hello, World!");
+        var user = UserCreator.CreateUser("ahah", "hehe", "Andrew", "Vetrov", "1234567890", "+77777777777");
+        Console.WriteLine(user.PasswordHash);
     }
 }
