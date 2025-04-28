ab0dbeac1069fd928e5943281a478a4bb04160f4
Andrew_Vetrov <user120715mts@gmail.com>
2025-04-28 17:49:26 +0300

    regex optimization

--- App/Practice3/User.cs
+++ App/Practice3/User.cs
@@ -4,6 +4,8 @@ namespace App.Practice3;
 
 public class User
 {
+    private static readonly Regex regex = new Regex(@"^(?:\+7|8|7)?[\s\-]?\(?\d{3}\)?[\s\-]?\d{3}[\s\-]?\d{2}[\s\-]?\d{2}$");
+    
     private readonly Guid id;
     private readonly DateTime registerDate;
     
@@ -28,7 +30,7 @@ public class User
     
     private static bool IsPhoneValid(string inputString) 
     {
-        MatchCollection matches = new Regex(@"^(?:\+7|8|7)?[\s\-]?\(?\d{3}\)?[\s\-]?\d{3}[\s\-]?\d{2}[\s\-]?\d{2}$").Matches(inputString);
+        var matches = regex.Matches(inputString);
 
         if (matches.Count != 1 || matches[0].Value.Length != inputString.Length) // второе условие - проверка на то, что в строке нет ничего лишнего
         {
