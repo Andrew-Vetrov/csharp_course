7ccfc68d35920dc5d49d07118872be75791ebc0c
Andrew_Vetrov <user120715mts@gmail.com>
2025-04-28 17:40:18 +0300

    review fixes

--- App/Practice2/PhoneNumber.cs
+++ App/Practice2/PhoneNumber.cs
@@ -1,12 +1,14 @@
+using System.Text;
 using System.Text.RegularExpressions;
 
 namespace App.Practice2;
 
 public class PhoneNumber
 {
+    private static readonly Regex regex = new Regex(@"^(?:\+7|8|7)?[\s\-]?\(?\d{3}\)?[\s\-]?\d{3}[\s\-]?\d{2}[\s\-]?\d{2}$");
     public static bool TryParsePhone(string inputString, out string parsedPhone) 
     {
-        MatchCollection matches = new Regex(@"^(?:\+7|8|7)?[\s\-]?\(?\d{3}\)?[\s\-]?\d{3}[\s\-]?\d{2}[\s\-]?\d{2}$").Matches(inputString);
+        var matches = regex.Matches(inputString);
 
         if (matches.Count != 1)
         {
@@ -14,7 +16,19 @@ public class PhoneNumber
             return false;
         }
 
-        parsedPhone = matches[0].Value;
+        var sb = new StringBuilder();
+        var match = matches[0].Value;
+        var len =  match.Length;
+        
+        for (var i = 0; i < len; i++)
+        {
+            if (Char.IsDigit(match[i]))
+            {
+                sb.Append(match[i]);
+            }
+        }
+
+        parsedPhone = sb.ToString();
         return true;
     }
 }
--- AppTests/Practice2/PhoneNumberTests.cs
+++ AppTests/Practice2/PhoneNumberTests.cs
@@ -4,30 +4,32 @@ namespace AppTests.Practice2;
 
 public class PhoneNumberTests
 {
-    [TestCase("+7 (912) 345-67-89", true)]
-    [TestCase("8(912)3456789", true)]
-    [TestCase("79123456789", true)]
-    [TestCase("9123456789", true)]
-    [TestCase("+7 912 345 67 89", true)]
-    [TestCase("7-912-345-67-89", true)]
-    [TestCase("7 812) 123-45-67", true)]
+    [TestCase("+7 (912) 345-67-89", "79123456789", true)]
+    [TestCase("8(912)3456789", "89123456789", true)]
+    [TestCase("79123456789", "79123456789",true)]
+    [TestCase("9123456789", "9123456789", true)]
+    [TestCase("+7 912 345 67 89", "79123456789", true)]
+    [TestCase("7-912-345-67-89", "79123456789", true)]
+    [TestCase("7 812) 123-45-67", "78121234567", true)]
 
-    [TestCase("+1 (123) 456-7890", false)]
-    [TestCase("912345", false)]
-    [TestCase("abcdefg", false)]
-    [TestCase("-7 (495) 123-45-67", false)]
-    [TestCase("", false)]
-    [TestCase("7 (912) 345-67-8", false)]
-    [TestCase("7 (912) 345-67-890", false)]
-    public void TestFunction(string number, bool expected)
+    [TestCase("+1 (123) 456-7890", "", false)]
+    [TestCase("6 (123) 456-7890", "", false)]
+    [TestCase("912345", "", false)]
+    [TestCase("abcdefg", "", false)]
+    [TestCase("-7 (495) 123-45-67", "", false)]
+    [TestCase("", "", false)]
+    [TestCase("7 (912) 345-67-8", "", false)]
+    [TestCase("7 (912) 345-67-890", "", false)]
+    
+    [TestCase("1234567890", "1234567890", true)]
+    public void TestFunction(string number, string expectedOutString, bool expected)
     {
-        string actualNumber;
-        var actual = PhoneNumber.TryParsePhone(number, out actualNumber);
+        var actual = PhoneNumber.TryParsePhone(number, out var actualNumber);
         Assert.That(actual, Is.EqualTo(expected));
         Console.WriteLine(actualNumber);
-        if (!String.IsNullOrEmpty(actualNumber))
+        if (!string.IsNullOrEmpty(actualNumber))
         {
-            Assert.That(actualNumber, Is.EqualTo(number));
+            Assert.That(actualNumber, Is.EqualTo(expectedOutString));
         }
     }
 }
