8bf6f5645acbe9f73dfff9ee089df10226d01c63
Andrew_Vetrov <user120715mts@gmail.com>
2025-04-30 07:56:25 +0300

    review fixes

--- App/Practice2/PhoneNumber.cs
+++ App/Practice2/PhoneNumber.cs
@@ -5,7 +5,7 @@ namespace App.Practice2;
 
 public class PhoneNumber
 {
-    private static readonly Regex regex = new Regex(@"^(?:\+7|8|7)?[\s\-]?\(?\d{3}\)?[\s\-]?\d{3}[\s\-]?\d{2}[\s\-]?\d{2}$");
+    private static readonly Regex regex = new Regex(@"(?<!\d)(?:\+7|8|7)?[\s-]*\(?\d{3}\)?[\s-]*\d{3}[\s-]*\d{2}[\s-]*\d{2}(?!\d)");
     public static bool TryParsePhone(string inputString, out string parsedPhone) 
     {
         var matches = regex.Matches(inputString);
@@ -15,9 +15,21 @@ public class PhoneNumber
             parsedPhone = null;
             return false;
         }
+        
+        var match = matches[0].Value;
+        var index = matches[0].Index - 1;
+        while (index >= 0 && char.IsWhiteSpace(inputString[index]))
+        {
+            index--;
+        }
+
+        if (index >= 0 && char.IsDigit(inputString[index]))
+        {
+            parsedPhone = null;
+            return false;
+        }
 
         var sb = new StringBuilder();
-        var match = matches[0].Value;
         var len =  match.Length;
         
         for (var i = 0; i < len; i++)
